3a459a4bd985fe0a23270bff1634219c1624bbb9
Chuck Fon Lee <clee@kinepolis.com>
2019-12-05 20:19:43 +0100

    day 5

--- AOCDay3/Program.cs
+++ AOCDay3/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
 
@@ -10,6 +12,8 @@ namespace AOCDay3 {
         private static int steps1 = 0;
         private static int steps2 = 0;
         static void Main(string[] args) {
+            Stopwatch s = new Stopwatch();
+            s.Start();
             var positions = "993,847,868,286,665,860,823,934,341,49,762,480,899,23,273,892,43,740,940,502,361,283,852,630,384,758,655,358,751,970,72,245,188,34,355,373,786,188,304,621,956,839,607,279,459,340,412,901,929,256,495,462,369,138,926,551,343,237,434,952,421,263,663,694,687,522,47,8,399,930,928,73,581,452,80,610,998,797,584,772,521,292,959,356,940,894,774,957,813,650,891,309,254,271,791,484,399,106,463,39,210,154,380,86,136,228,284,267,195,727,739,393,395,703,385,483,433,222,945,104,605,814,656,860,474,672,812,789,29,256,857,436,927,99,171,727,244,910,347,789,49,598,218,834,574,647,185,986,273,363,848,531,837,433,795,923,182,915,367,347,867,789,776,568,969,923,765,589,772,715,38,968,845,327,721,928,267,94,763,799,946,130,649,521,569,139,584,27,823,918,450,390,149,237,696,258,757,810,216,202,966,157,702,623,740,560,932,587,197,56,695,439,655,576,695,176,800,374,806,969,664,216,170,415,485,188,444,613,728,508,644,289,831,978,711,973,3,551,377,114,15,812,210,829,536,883,843,427,311,680,482,69,125,953,896,85,376,683,374,415,3,843,802,124,299,345,696,276,87,98,619,321,348,806,789,657,590,747,477,251,854,351,82,982,906,94,285,756,737,377,951,126,852,751,946,696,44,709,851,364,222";
             var numbers = positions.Split(',');
             var sum = 0;
@@ -20,34 +24,39 @@ namespace AOCDay3 {
             var lines = File.ReadAllLines("input.txt");
             FillLine(lines[0],line1, steps1);
             FillLine(lines[1], line2, steps2);
-            line1.RemoveAt(0);
-            line2.RemoveAt(0);
+            //line1.RemoveAt(0);
+            //line2.RemoveAt(0);
             var closestCrossing = 99999;
             var line1Steps = 99999;
             var line2Steps = 99999;
             var totalSteps = 0;
-            foreach (var pos in line1) {
-                foreach (var pos2 in line2) {
-                    if (pos.x == pos2.x && pos.y == pos2.y) {
-                        //Console.WriteLine(Math.Abs(pos.x) + Math.Abs(pos.y));
-                        //if (closestCrossing > Math.Abs(pos.x) + Math.Abs(pos.y)) {
-                        //    closestCrossing = Math.Abs(pos.x) + Math.Abs(pos.y);
-                        //}
-                        var steps = line1.Distinct().ToList();
-                        var steps2 = line2.Distinct().ToList();
+            //foreach (var pos in line1) {
+            //    foreach (var pos2 in line2) {
+            //        if (pos.x == pos2.x && pos.y == pos2.y) {
+            //Console.WriteLine(Math.Abs(pos.x) + Math.Abs(pos.y));
+            //if (closestCrossing > Math.Abs(pos.x) + Math.Abs(pos.y)) {
+            //    closestCrossing = Math.Abs(pos.x) + Math.Abs(pos.y);
+            //}
+            //            var steps = line1.Distinct().ToList();
+            //            var steps2 = line2.Distinct().ToList();
 
-                        var actualSteps = steps.IndexOf(pos);
-                        var actualSteps2 = steps2.IndexOf(pos2);
-                        if (actualSteps < line1Steps && actualSteps2 < line2Steps) {
-                            totalSteps = actualSteps + actualSteps2;
-                            line1Steps = actualSteps;
-                            line2Steps = actualSteps2;
-                        }
-                    }
-                }
-            }
+            //            var actualSteps = steps.IndexOf(pos);
+            //            var actualSteps2 = steps2.IndexOf(pos2);
+            //            if (actualSteps < line1Steps && actualSteps2 < line2Steps) {
+            //                totalSteps = actualSteps + actualSteps2;
+            //                line1Steps = actualSteps;
+            //                line2Steps = actualSteps2;
+            //            }
+            //        }
+            //    }
+            //}
+            Console.WriteLine(line1.Count());
+            var intersections = line1.Intersect(line2);
+            Console.WriteLine(intersections.FirstOrDefault().x + intersections.FirstOrDefault().y);
             //var closestCrossing =line1.Find(position => line2.Exists(pos => pos.x == position.x && pos.y == position.y));
             Console.WriteLine(totalSteps);
+            s.Stop();
+            Console.Write(s.ElapsedMilliseconds / 1000);
             //Console.ReadLine();
             //Console.WriteLine(line1.Count);
             //Console.WriteLine(line2.Count);
@@ -102,12 +111,16 @@ namespace AOCDay3 {
         }
     }
 
-    internal class Position: IComparable<Position>{
+    internal class Position: IEquatable<Position>{
         public int x { get; set; }
         public int y { get; set; }
 
         public int CompareTo(Position otherPos) {
             return x - otherPos.x + y - otherPos.y;
+        }
+
+        public bool Equals(Position other) {
+            return true;
         }
     }
 }
---
+++ AOCDay5/Program.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AOCDay5 {
+    class Program {
+        private static int[] allInputs = new int[999999];
+        private static int index = 0;
+        private static bool running = true;
+        static void Main(string[] args) {
+            calc();
+            Console.ReadLine();
+        }
+
+        private static void calc() {
+            var lines = File.ReadAllLines("input.txt");
+            allInputs = lines[0].Split(',').Select(numberAsString => Int32.Parse(numberAsString.ToString())).ToArray();
+            while (running) {
+                //allInputs[1] = 12;
+                //allInputs[2] = 2;
+                var instruction = allInputs[index];
+                var instructionAsString = instruction.ToString();
+                string opcode;
+                if (instructionAsString.Length != 1) { 
+                    opcode = instructionAsString.Substring(instructionAsString.Length - 2, 2);
+                } else {
+                    opcode = instructionAsString;
+                }
+                List<int> modes;
+                if (instructionAsString.Length != 1) {
+                    modes = instructionAsString.Substring(0, instructionAsString.Length - 2).Reverse().Select(mode => Int32.Parse(mode.ToString())).ToList();
+                while (modes.Count() != 3) {
+                    modes.Add(0);
+                }
+                } else {
+                    modes = new List<int>();
+                    while (modes.Count() != 3) {
+                        modes.Add(0);
+                    }
+                }
+                switch (opcode.Replace("0","")) {
+                    case "1": opcode1(modes.ToArray()); break;
+                    case "2": opcode2(modes.ToArray()); break;
+                    case "3": opcode3(); break;
+                    case "4": opcode4(); break;
+                    case "5": opcode5(modes.ToArray()); break;
+                    case "6": opcode6(modes.ToArray()); break;
+                    case "7": opcode7(modes.ToArray()); break;
+                    case "8": opcode8(modes.ToArray()); break;
+                    case "99": opcode99(); break;
+                }
+            }
+            
+        }
+
+        private static int[] GetParameters(int amountOfParameters) {
+            List<int> numbers = new List<int>();
+            for (int number= 1;number <= amountOfParameters; number++) {
+                numbers.Add(allInputs[index + number]);
+            }
+            return numbers.ToArray();
+        }
+
+        private static void opcode1(int[] modes) {
+            int[] inputs = GetParameters(3);
+            index += 4;
+
+            for (int i=0;i<modes.Length-1; i++) {
+                if (modes[i] == 1) {
+                    
+                } else if (modes[i] == 0) {
+                    int b = inputs[i];
+                    inputs[i] = allInputs[b];
+                }
+            }
+            var toPutIn = inputs[0] + inputs[1];
+            int a = inputs[2];
+            allInputs[a] = toPutIn;
+        }
+        private static void opcode2(int[] modes) {
+            int[] inputs = GetParameters(3);
+            index += 4;
+
+            for (int i = 0; i < modes.Length-1; i++) {
+                if (modes[i] == 1) {
+                    
+                } else 
+                if (modes[i] == 0) {
+                    int b = inputs[i];
+                    inputs[i] = allInputs[b];
+                }
+            }
+            var toPutIn = inputs[0] * inputs[1];
+            int a = inputs[2];
+            allInputs[a] = toPutIn;
+        }
+        private static void opcode99() {
+            running = false;
+        }
+        private static void opcode3() {
+            Console.WriteLine("Write a god damn number!");
+            int numberInput = Int32.Parse(Console.ReadLine().ToString());
+            int[] parameters = GetParameters(1);
+            index += 2;
+            int a = parameters[0];
+            allInputs[a] = numberInput;
+        }
+        private static void opcode4() {
+            int[] parameters = GetParameters(1);
+            index += 2;
+            int a = parameters[0];
+            Console.WriteLine(allInputs[a]);
+        }
+
+        private static void opcode5(int[] modes) {
+            int[] inputs = GetParameters(2);
+
+            for (int i = 0; i < modes.Length - 1; i++) {
+                if (modes[i] == 1) {
+
+                }
+                else
+                if (modes[i] == 0) {
+                    int b = inputs[i];
+                    inputs[i] = allInputs[b];
+                }
+            }
+
+            if (inputs[0] != 0) {
+                index = inputs[1];
+            } else {
+                index += 3;
+            }
+        }
+        private static void opcode6(int[] modes) {
+            int[] inputs = GetParameters(2);
+
+            for (int i = 0; i < modes.Length - 1; i++) {
+                if (modes[i] == 1) {
+
+                }
+                else
+                if (modes[i] == 0) {
+                    int b = inputs[i];
+                    inputs[i] = allInputs[b];
+                }
+            }
+
+            if (inputs[0] == 0) {
+                index = inputs[1];
+            }
+            else {
+                index += 3;
+            }
+        }
+        private static void opcode7(int[] modes) {
+            int[] inputs = GetParameters(3);
+            index += 4;
+
+            for (int i = 0; i < modes.Length - 1; i++) {
+                if (modes[i] == 1) {
+
+                }
+                else
+                if (modes[i] == 0) {
+                    int b = inputs[i];
+                    inputs[i] = allInputs[b];
+                }
+            }
+
+            if (inputs[0] < inputs[1]) {
+                int a = inputs[2];
+                allInputs[a] = 1;
+            }
+            else {
+                int a = inputs[2];
+                allInputs[a] = 0;
+            }
+        }
+        private static void opcode8(int[] modes) {
+            int[] inputs = GetParameters(3);
+            index += 4;
+
+            for (int i = 0; i < modes.Length - 1; i++) {
+                if (modes[i] == 1) {
+
+                }
+                else
+                if (modes[i] == 0) {
+                    int b = inputs[i];
+                    inputs[i] = allInputs[b];
+                }
+            }
+
+            if (inputs[0] == inputs[1]) {
+                int a = inputs[2];
+                allInputs[a] = 1;
+            }
+            else {
+                int a = inputs[2];
+                allInputs[a] = 0;
+            }
+        }
+    }
+}
